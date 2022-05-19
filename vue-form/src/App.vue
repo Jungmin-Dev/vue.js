4ab03a0c391cdb309b66592035c80e1834233408
Jungmin Kim <kimjungmin__@naver.com>
2022-05-19 14:35:32 +0900

    사용자 입력 폼 만들기

---
+++ vue-form/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input type="text" id="username" v-model="username" />
+    </div>
+    <div>
+      <label for="password">password: </label>
+      <input type="password" id="password" v-model="password" />
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitForm: function () {
+      console.log(this.username, this.password);
+      const url = "https://jsonplaceholder.typicode.com/users";
+      const data = {
+        username: this.username,
+        password: this.username,
+      };
+      axios
+        .post(url, data)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+};
+</script>
+
+<style></style>
